1b7bde5c75147302035ac5ac857cfb89f36badff
rodrigo <rodrigopi1989@gmail.com>
2019-07-05 15:17:30 -0300

    percy tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom'
 import App from '../App'
 import { exportAllDeclaration } from '@babel/types'
 const puppeteer = require('puppeteer')
-
 const dappeteer = require('dappeteer')
+const { percySnapshot } = require('@percy/puppeteer')
 
 jest.setTimeout(60000)
 
@@ -27,30 +27,29 @@ describe('Apps test ', () => {
     const div = await page.waitForSelector('#BlocksContainer')
     const element = await page.waitForSelector('#BlocksTitle')
     const text = await page.evaluate(element => element.textContent, element)
-
+    await percySnapshot(page, 'Blocks Section')
     expect(text).toBe('Latest Blocks')
-  }),
-    it('renders blocks section', async () => {
-      const tr = await page.waitForSelector('#blockrow3')
-      const blockNumberSpan = await page.waitForSelector(
-        '#blockNumberblockrow3'
-      )
-      await page.click('#blockrow3')
-      const blockNumberText = await page.evaluate(
-        blockNumberSpan => blockNumberSpan.textContent,
-        blockNumberSpan
-      )
-      const transactionTitle = await page.waitForSelector('#TransactionTitle')
-      const transactionTitleText = await page.evaluate(
-        transactionTitle => transactionTitle.textContent,
-        transactionTitle
-      )
+  })
 
-      expect(transactionTitleText).toBe(
-        'Transactions From Block: ' + blockNumberText
-      )
+  it('renders transactions section', async () => {
+    const tr = await page.waitForSelector('#blockrow3')
+    const blockNumberSpan = await page.waitForSelector('#blockNumberblockrow3')
+    await page.click('#blockrow3')
+    const blockNumberText = await page.evaluate(
+      blockNumberSpan => blockNumberSpan.textContent,
+      blockNumberSpan
+    )
+    const transactionTitle = await page.waitForSelector('#TransactionTitle')
+    const transactionTitleText = await page.evaluate(
+      transactionTitle => transactionTitle.textContent,
+      transactionTitle
+    )
+    await percySnapshot(page, 'Transaction Section')
+    expect(transactionTitleText).toBe(
+      'Transactions From Block: ' + blockNumberText
+    )
 
-      browser.close()
-    })
+    browser.close()
+  })
   16000
 })
